90c950a062493e482461b0cc9c76c48ce48d7c9a
PranavMehrotra <87844730+PranavMehrotra@users.noreply.github.com>
2021-09-03 08:22:09 +0530

    Update code.js

--- code.js
+++ code.js
@@ -1,1 +1,31 @@
-#
+function fade(z){
+    var a=document.getElementById("box_1");
+    var b=document.getElementById("box_2");
+    var c= document.getElementById("box_3");
+    var d= document.getElementById("box_4");
+        if(z==1){
+            a.style.display= "flex";
+            b.style.display= "none";
+            c.style.display= "none";
+            d.style.display= "none";
+        }
+        else if(z==2){
+            b.style.display= "flex";
+            a.style.display= "none";
+            c.style.display= "none";
+            d.style.display= "none";
+        }
+        else if(z==3){
+            c.style.display= "flex";
+            b.style.display= "none";
+            a.style.display= "none";
+            d.style.display= "none";
+        }
+        else if(z==4){
+            d.style.display= "flex";
+            b.style.display= "none";
+            c.style.display= "none";
+            a.style.display= "none";
+        }
+     
+}
